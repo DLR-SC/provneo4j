cbafda6304e4b203c56978ed7e2d7f7ca0d4c704
Stefan B <Stefan.bie@live.de>
2016-09-05 13:28:13 +0200

    changed to neo4j connector

--- provstore/api.py
+++ provstore/api.py
@@ -1,49 +1,12 @@
 import os
 import json
-import requests
-from copy import copy
 from prov.model import ProvDocument
 from provstore.document import Document
-
-
-MAX_RETRIES = 3
-
-
-class ProvStoreException(Exception):
-    pass
-
-
-class NotFoundException(ProvStoreException):
-    pass
-
-
-class RequestTimeoutException(ProvStoreException):
-    pass
-
-
-class InvalidCredentialsException(ProvStoreException):
-    pass
-
-
-class ForbiddenException(ProvStoreException):
-    pass
-
-
-class InvalidDataException(ProvStoreException):
-    pass
-
-
-class UnprocessableException(ProvStoreException):
-    pass
-
-
-class DocumentInvalidException(ProvStoreException):
-    pass
-
+from provstore.connectors import  *
 
 class Api(object):
     """
-    Main ProvStore API client object
+    Main Neo4J-Prov API client object
 
     Most functions are not used directly but are instead accessed by functions of the Document, BundleManager and Bundle
     objects.
@@ -54,31 +17,16 @@ class Api(object):
 
     .. note::
        The username and api_key parameters can also be omitted in which case the client will look for
-       **PROVSTORE_USERNAME** and **PROVSTORE_API_KEY** environment variables.
+       **NEO4J_USERNAME** and **NEO4J_API_KEY** environment variables.
 
-    To read public documents no credentials need be provided.
     """
-    FORMAT_MAP = {
-        'json': 'application/json'
-    }
 
     def __init__(self,
                  username=None,
                  api_key=None,
                  base_url=None):
-
-        if base_url is None:
-            self.base_url = 'https://provenance.ecs.soton.ac.uk/store/api/v0'
-        else:
-            self.base_url = base_url.rstrip('/')
-
-        self._username = username
-        self._api_key = api_key
-
-        if not self._username:
-            self._username = os.environ.get('PROVSTORE_USERNAME', None)
-        if not self._api_key:
-            self._api_key = os.environ.get('PROVSTORE_API_KEY', None)
+        self._connector = Neo4J()
+        self._connector.connect(base_url=base_url, username=username, user_password=api_key)
 
     def __eq__(self, other):
         if not isinstance(other, Api):
@@ -93,118 +41,39 @@ class Api(object):
     def document(self):
         return Document(self)
 
-    @property
-    def headers(self):
-        headers = dict()
-
-        headers['Accept'] = 'application/json'
-
-        if self._username and self._api_key:
-            headers['Authorization'] = self._authorization_header
-
-        return headers
-
-    def _request(self, method, url, retries=0, *args, **kwargs):
-        url = self.base_url + url
-
-        try:
-            kwargs.update({'timeout': 30})
-            r = requests.request(method, url, **kwargs)
-        except requests.exceptions.Timeout:
-            if retries < MAX_RETRIES:
-                return self._request(method, *args, retries=retries+1, **kwargs)
-            raise RequestTimeoutException()
-
-        if r.status_code == 500:
-            raise ProvStoreException()
-        elif r.status_code == 422:
-            raise UnprocessableException()
-        elif r.status_code == 410:
-            raise DocumentInvalidException()
-        elif r.status_code == 404:
-            raise NotFoundException()
-        elif r.status_code == 403:
-            raise ForbiddenException()
-        elif r.status_code == 401:
-            raise InvalidCredentialsException()
-        elif r.status_code == 400:
-            raise InvalidDataException()
-        else:
-            # Fallback, this should not happen!
-            r.raise_for_status()
-
-        return r
-
-    @property
-    def _authorization_header(self):
-        return "ApiKey %s:%s" % (self._username, self._api_key)
-
     def get_document_prov(self, document_id, prov_format=ProvDocument):
-        if prov_format == ProvDocument:
-            extension = 'json'
-        else:
-            extension = prov_format
+        raise NotImplementedException()
 
-        r = self._request('get', "/documents/%i.%s" % (document_id, extension),
-                          headers=self.headers)
-
-        if prov_format == ProvDocument:
-            return ProvDocument.deserialize(content=r.content)
-        else:
-            return r.content
 
     def get_document_meta(self, document_id):
-        r = self._request('get', "/documents/%i/" % document_id,
-                          headers=self.headers)
-        return r.json()
+        raise NotImplementedException()
+
 
     def post_document(self, prov_document, prov_format, name, public=False):
-        headers = copy(self.headers)
-        headers.update({'Content-type': self.FORMAT_MAP[prov_format]})
 
-        r = self._request('post', '/documents/',
-                          data=json.dumps({
-                              'content': prov_document,
-                              'public':  public,
-                              'rec_id':  name
-                          }),
-                          headers=headers)
-        return r.json()
+        if prov_format == "json":
+             prov_document = ProvDocument.deserialize(content=prov_document)
+        else:
+            raise Exception("Not supported format ")
+
+        return self._connector.post_document(prov_document,name)
+
+
 
     def add_bundle(self, document_id, prov_bundle, identifier):
-        headers = copy(self.headers)
-        headers.update({'Content-type': 'application/json'})
 
-        self._request('post', "/documents/%i/bundles/" % document_id,
-                      data=json.dumps({
-                          'content': json.loads(prov_bundle),
-                          'rec_id':  unicode(identifier)
-                      }),
-                      headers=headers)
+        raise NotImplementedException()
 
-        return True
 
     def get_bundles(self, document_id):
-        r = self._request('get', "/documents/%i/bundles/" % document_id,
-                          headers=self.headers)
 
-        return r.json()['objects']
+        raise NotImplementedException()
 
     def get_bundle(self, document_id, bundle_id, prov_format=ProvDocument):
-        if prov_format == ProvDocument:
-            extension = 'json'
-        else:
-            extension = prov_format
 
-        r = self._request('get', "/documents/%i/bundles/%i.%s" % (document_id, bundle_id, extension),
-                          headers=self.headers)
-
-        if prov_format == ProvDocument:
-            return ProvDocument.deserialize(content=r.content)
-        else:
-            return r.content
+        raise NotImplementedException()
 
     def delete_document(self, document_id):
-        self._request('delete', "/documents/%i/" % document_id,
-                      headers=self.headers)
+
+        raise NotImplementedException()
         return True
