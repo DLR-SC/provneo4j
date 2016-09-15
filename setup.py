8a68d31439dca659edcee5ef008bd50aed6d2359
Andreas Schreiber <andreas.schreiber@dlr.de>
2016-09-15 21:44:42 +0200

    Missing packages added.

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@
 from setuptools import setup
 
 setup(name='provneo4j-api',
-    version='0.1',
+    version='0.1.1',
     description='Neo4j PROV API client',
-    author='Sam Millar, Stefan Bieliauskas',
+    author='DLR, Sam Millar, Stefan Bieliauskas',
     author_email='sam@millar.io, sb@conts.de',
     url='https://github.com/DLR-SC/provneo4j-api',
-    packages=['provneo4j'],
+    packages=['provneo4j', 'provneo4j.connectors', 'provneo4j.connectors.neo4j_rest', 'provneo4j.tests'],
     install_requires=[
         'prov>=1.0.0',
         'requests',
-        'neo4jrestclient'
+        'neo4jrestclient',
+        'lxml'
     ],
     license="MIT",
     test_suite='provneo4j.tests',
